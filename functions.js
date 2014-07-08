44bed81f8cc98ac78ba97925ea9536d4655eaba8
TigerControl <nat.morcos@gmail.com>
2014-07-07 23:19:09 -0400

    adding some direction

--- functions.js
+++ functions.js
@@ -2,7 +2,6 @@
  * REST API for CDTO-09
  */
 
- 
 function validatePoolId(pid) {
     if ([1,2,3,4].indexOf(Number(pid) == -1)) {
         throw 'Invalid Pool Number';
@@ -12,20 +11,4 @@ function validatePoolId(pid) {
 exports.assign = function(req, res){
     //TODO return a unique pool / rank
     res.send("Heyyyy we'd give you numbers here.")
-}
-
-exports.show = function(req, res){
-    //TODO
-    res.send("Hey, we're here!");
-}
-
-exports.getInstrument = function(req, res){
-    var pid = req.params.pid;
-    try {
-        validatePoolId(pid);
-        res.send("your instrumend id is" + pid);
-        //TODO
-    } catch (e) {
-        res.json(400, {error:e});
-    }
 }
