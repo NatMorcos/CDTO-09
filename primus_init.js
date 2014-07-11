e2157716a17b552a01db70d2048644485766248a
TigerControl <nat.morcos@gmail.com>
2014-07-11 02:04:28 -0400

    events firing back and forth -- 2 clients

--- primus_init.js
+++ primus_init.js
@@ -5,24 +5,40 @@ module.exports = function(primus){
 	primus.on('connection', function(spark){
 	  
 		spark.on('noise maker join', function(callback){
-			spark.join('noiseyRoom', function(){
+      spark.join('noiseyRoom', function(){
 				callback && callback();
 				console.log('noise maker is here!!')
 			});
 		});
 
-		spark.on('sound board join', function(callback){
+		spark.on('soundboard join', function(callback){
+      console.log('inside sound board join');
 			spark.join('soundControlRoom', function(){
 				callback && callback();
-				console.log('a new sound board joined')
+				console.log('a new sound board joined. Hawt diggity.');
 			});
 		});
 
-		// spark.on('drumkit increase volume', function(data, callback){
-		// 	if(functions.canPlayNow(data.rank)){
-		// 		primus.room('noiseyRoom').send('eventName', data);
-		// 	}
-		// });
+    //in these, eventually data will contain the pool & rank
+    spark.on('hit note harder', function(data){
+      spark.room('noiseyRoom').send('hit note harder', data);
+      console.log('hit note harder event received');
+    });
+
+    spark.on('hit note softer', function(data){
+      spark.room('noiseyRoom').send('hit note softer', data);
+      console.log('hit note softer event received');
+    });
+
+    spark.on('increase note', function(data){
+      spark.room('noiseyRoom').send('increase note', data);
+      console.log('increase note event received');
+    });
+
+    spark.on('decrease note', function(data){
+      spark.room('noiseyRoom').send('decrease note', data);
+      console.log('increase note event received');
+    });
 
 	});
 
